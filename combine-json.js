67bf4907764b03f78d95b808bd1df04195b23db2
YahYa <y.quazbari@rmaassurance.com>
2024-05-09 12:02:59 +0100

    2nd commit

--- combine-json.js
+++ combine-json.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const dbData = require('./db.json');
-const subscriberData = require('./subscriber-step.json');
-
-const combinedData = { ...dbData, ...subscriberData };
+const dbData = require('./selects_dataset/db.json');
+const subscriberData = require('./selects_dataset/subscriber-step.json');
+const vehicleData = require('./selects_dataset/vehicle-step.json');
+const combinedData = { ...dbData, ...subscriberData, ...vehicleData };
 
 fs.writeFileSync('./combined.json', JSON.stringify(combinedData, null, 2));
